665f12eec80285b307f84acaafc57ad22bc676f4
z008hy <z008hy@163.com>
2017-04-10 16:23:17 +0800

    初次上传

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  globals: {
+    $: false,
+    _: false,
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['off', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    'import/no-unresolved': [0, {commonjs: true, amd: true}],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-param-reassign': ['off', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }]
+  }
+}
---
+++ src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="20" :offset="2">
+      <h3>最新资源</h3>
+      <el-table :data="resourcesTableData" border style="width: 100%">
+        <el-table-column label="资源类型">
+          <template scope="scope">
+            <span>{{ scope.row.type }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="更新日期">
+          <template scope="scope">
+            <el-icon name="time"></el-icon>
+            <span>{{ scope.row.date }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="资源描述">
+          <template scope="scope">
+            <span>{{ scope.row.description }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="资源链接">
+          <template scope="scope">
+            <span>{{ scope.row.link }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="资源密码">
+          <template scope="scope">
+            <span>{{ scope.row.password }}</span>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        resourcesTableData: [
+          { date: '20170410', type: '百度云', description: 'NodeJs+JavaScript视频教程', link: 'http://pan.baidu.com/s/1sjQPDrn', password: 'a54k' },
+          { date: '20170410', type: '百度云', description: 'Node.js 4.x 入门实战视频课程', link: 'http://pan.baidu.com/s/1c2ADcLa', password: 'rkwe' },
+          { date: '20170410', type: '百度云', description: 'Node.js 4.x 核心技术之 Buffer精通', link: 'http://pan.baidu.com/s/1i5C4H8p', password: 'cv71' },
+          { date: '20170410', type: '百度云', description: 'Node.js - Express 4.x框架高级实战', link: 'http://pan.baidu.com/s/1o8AY9V8', password: 'ens8' },
+        ],
+      };
+    },
+  };
+</script>
